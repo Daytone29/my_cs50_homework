c1960ad1de22a7668935c3fefdf5c3507bcebc5a
Daytone29 <103832907+Daytone29@users.noreply.github.com>
2023-10-07 18:02:48 +0300

    Create mario.c

---
+++ LAB_0/mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int in;
+//We select possible options for incorrect input
+    do
+    {
+        in = get_int("Height: "); //option for input from cs50 library
+    }
+    while (9 < in || in <= 0);
+  
+    for (int i = 1; i <= in; ++i)  //create max hight from input
+    {
+        for (int j = in; j > 0; --j)  //count the number of spacec and heshes
+            if (j > i)
+                printf("%c", ' ');
+            else
+                printf("%c", '#');
+        putchar('\n');  //function dor division`s rows
+    }
+}
